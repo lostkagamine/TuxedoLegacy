be7562b3f736e5d143869eca1957c5ac014fcadf
ry00001 <dar.gargano@gmail.com>
2017-07-24 15:41:34 +0100

    ok]

--- bot.py
+++ bot.py
@@ -11,9 +11,9 @@ with open("config.json") as f:
 token = config.get('BOT_TOKEN')
 prefix = config.get('BOT_PREFIX')
 
-redis_host = 'localhost'
-redis_port = 6379
-redis_db = 0
+redis_host = config["redis"]["host"]
+redis_port = config["redis"]["port"]
+redis_db = config["redis"]["db"]
 
 try:
     redis_conn = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db)
