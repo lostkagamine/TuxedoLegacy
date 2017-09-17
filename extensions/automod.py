8e0dfd9bdec9de142c8788360e502205c9cb99a4
ry00000 <dar.gargano@gmail.com>
2017-09-17 17:17:32 +0000

    yay more fixes to automod

--- extensions/automod.py
+++ extensions/automod.py
@@ -3,12 +3,14 @@ from discord.ext import commands
 import rethinkdb as r
 import re
 
-regex = r"(?:discord(?:(?:\.|.?dot.?)gg|app|ls|me(?:\.|.?dot.?)com\/invite)\/(([\w]{1,}|[a-zA-Z0-9]{1,})))"
+regex = r"(?:discord(?:(?:\.|.?dot.?)gg|app(?:\.|.?dot.?)com\/invite)\/(([\w]{1,}|[a-zA-Z0-9]{1,})))"
+
 
 class AutoMod:
     def __init__(self, bot):
         self.bot = bot
         self.conn = bot.conn
+
         @bot.listen('on_message')
         async def on_message(msg):
             if isinstance(msg.channel, discord.DMChannel) or isinstance(msg.channel, discord.GroupChannel):
@@ -23,15 +25,21 @@ class AutoMod:
                 lambda a: a['guild'] == str(msg.guild.id)).run(self.conn))[0]
             if "enable_automod" not in settings.keys():
                 return
-            
+
             if settings['enable_automod']:
                 # do cool stuff
                 inv = re.search(regex, msg.content)
-
-                # print(inv)
                 if not inv:
                     return
-                
+
+                try:
+                    invite = await self.bot.get_invite(inv[0])
+                except discord.errors.NotFound:
+                    return # Invalid invite
+
+                if invite.guild.id == msg.guild.id:
+                    return
+
                 author_perms = msg.author.permissions_in(msg.channel)
 
                 if author_perms.kick_members or author_perms.ban_members or author_perms.administrator:
@@ -45,6 +53,7 @@ class AutoMod:
                 if msg.channel.permissions_for(msg.guild.me).manage_messages:
                     await msg.delete()
                     await msg.channel.send(f'{msg.author}, please do not advertise here.')
+
 
 def setup(bot):
     bot.add_cog(AutoMod(bot))
