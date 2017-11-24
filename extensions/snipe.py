2b169e7c34f7060843fdc97df4b3e24767c977d7
ry00001 <dar.gargano@gmail.com>
2017-11-24 20:14:28 +0000

    I made snipe saner

--- extensions/snipe.py
+++ extensions/snipe.py
@@ -18,6 +18,11 @@ class Snipe:
             if editdistance.eval(before.content, after.content) >= 10:
                 self.snipes[before.channel.id] = [before, after]
 
+    def sanitise(self, string):
+        if len(string) > 1024:
+            string = string[0:1021] + "..."
+        return string
+
     @commands.command(description='"Snipes" someone\'s message that\'s been edited or deleted.')
     async def snipe(self, ctx):
         '"Snipes" someone\'s message that\'s been edited or deleted.'
@@ -32,8 +37,8 @@ class Snipe:
         if type(snipe) == list: # edit snipe
             emb.set_author(name=str(snipe[0].author), icon_url=snipe[0].author.avatar_url)
             emb.colour = snipe[0].author.colour
-            emb.add_field(name='Before', value=snipe[0].content, inline=False)
-            emb.add_field(name='After', value=snipe[1].content, inline=False)
+            emb.add_field(name='Before', value=self.sanitise(snipe[0].content), inline=False)
+            emb.add_field(name='After', value=self.sanitise(snipe[1].content), inline=False)
         else: # delete snipe
             emb.set_author(name=str(snipe.author), icon_url=snipe.author.avatar_url)
             emb.description = snipe.content
