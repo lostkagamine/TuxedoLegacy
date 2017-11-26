97749052636a54cef8266d01670aeb5badf3676d
ry00001 <dar.gargano@gmail.com>
2017-11-26 17:22:20 +0000

    VPS weirdness

--- start.py
+++ start.py
@@ -6,8 +6,6 @@ with open('config.json') as f:
 ## STARTS TUXEDO ##
 # PM2 has failed me. 
 
-print(f'## Tuxedo ##\nversion {ver}\nBy ry00001 (ry00001#3487), HexadecimalPython (Hexadecimal™#0910) and Devoxin (Devoxin#0387).\nThis is free and open-source software.')
-
 while True:
     os.system(f'{sys.executable} bot.py')
     print('Bot crashed! Rebooting...')
