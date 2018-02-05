ecd1e96e42d38ca65fa64937c3c8adbff3d5af0b
Anurag <ctrldelme@gmail.com>
2018-02-05 21:50:05 +0530

    added Youtube lookup command
    all requirements satisfied already

---
+++ extensions/youtube.py
@@ -0,0 +1,26 @@
+from urllib import parse
+
+import requests
+from bs4 import BeautifulSoup
+from discord.ext import commands
+
+
+class Youtube:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True, aliases=['yt', 'vid', 'video'])
+    async def youtube(self, ctx, *, msg):
+        f"""Search for videos on YouTube.
+        Usage:
+        {ctx.prefix}yt (query)
+        """
+        search = parse.quote(msg)
+        response = requests.get(
+            "https://www.youtube.com/results?search_query={}".format(search), verify=False).text
+        result = BeautifulSoup(response, "html.parser")
+        await ctx.send("https://www.youtube.com{}".format(result.find_all(attrs={'class': 'yt-uix-tile-link'})[0].get('href')))
+
+
+def setup(bot):
+    bot.add_cog(Youtube(bot))
