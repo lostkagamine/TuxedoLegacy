7fe31c478beaa917fa59e4e9a6a49aea2cbcffdd
Anurag <ctrldelme@gmail.com>
2018-02-07 14:27:03 +0530

    Tux hates urllib :<

--- extensions/jisho.py
+++ extensions/jisho.py
@@ -3,52 +3,45 @@
 """jisho.org query command."""
 
 import urllib.parse
-import random
+import requests
 
 import discord
 from discord.ext import commands
 
-BASE_URL = "http://jisho.org/api/v1/search/words?"
+BASE_URL = "http://jisho.org/api/v1/search/words"
 
 
 class Jisho:
     """A Japanese translation command."""
 
     @commands.command(aliases=["jp"])
-    async def jisho(self, ctx, query, *args):
+    async def jisho(self, ctx, query):
         f"""Translate a word into Japanese.
 
         Example usage:
         {ctx.prefix}jisho test
         """
-        query = f"{query} {' '.join(args)}"
-        params = urllib.parse.urlencode({"keyword": query})
-        url = BASE_URL + params
-        async with ctx.bot.session.get(url) as response:
-            if response.status == 200:
-                data = await response.json()
+        with requests.get(BASE_URL, params={"keyword": query}) as response:
+            data = response.json()
 
-                if not data["data"]:
-                    await ctx.send("No matching result found on jisho.org.")
+            if not data["data"]:
+                await ctx.send("No matching result found on jisho.org.")
 
-                jap = data["data"][0]["japanese"][0]
-                senses = data["data"][0]["senses"][0]
-                defs = ", ".join(senses["english_definitions"])
-                tags = senses["tags"]
+            jap = data["data"][0]["japanese"][0]
+            senses = data["data"][0]["senses"][0]
+            defs = ", ".join(senses["english_definitions"])
+            tags = senses["tags"]
 
-                embed = discord.Embed(color=discord.Color.blurple())
-                embed.add_field(name="Kanji", value=str(jap.get("word")))
-                embed.add_field(name="Kana", value=str(
-                    jap.get("reading")), inline=False)
-                embed.add_field(name="English", value=defs, inline=False)
-                embed.add_field(name="Tags", value=tags)
-                embed.set_thumbnail(
-                    url="https://www.tofugu.com/images/learn-japanese/jisho-org-9a549ffd.jpg")
-                embed.set_footer(text="Powered by Jisho.org")
-                await ctx.send(embed=embed)
-
-            else:
-                await ctx.send("Couldn't reach Jisho.org")
+            embed = discord.Embed(color=discord.Color.blurple())
+            embed.add_field(name="Kanji", value=str(jap.get("word")))
+            embed.add_field(name="Kana", value=str(
+                jap.get("reading")), inline=False)
+            embed.add_field(name="English", value=defs, inline=False)
+            embed.add_field(name="Tags", value=tags)
+            embed.set_thumbnail(
+                url="https://www.tofugu.com/images/learn-japanese/jisho-org-9a549ffd.jpg")
+            embed.set_footer(text="Powered by Jisho.org")
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
