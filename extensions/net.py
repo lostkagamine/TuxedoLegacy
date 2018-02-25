2c396e7101cd00af4a8a0c412a741ef6cc85dafe
stupid cat <kirkbenm@gmail.com>
2018-02-25 10:29:43 -0700

    fix hole in net

--- extensions/net.py
+++ extensions/net.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 # look at this net
 class Net(BaseException):
     # that i just found
-    @commands.command(pass_context=True, aliases=['net'])
+    @commands.command(pass_context=True)
     async def net(self, ctx, *, msg):
         """net"""
         # when i say go, be ready to throw
