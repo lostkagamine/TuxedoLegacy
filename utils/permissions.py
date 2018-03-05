a499d18397b6775fa15b50ac8243bf25f85d69ee
eye-sigil <ustin.a.h+github@gmail.com>
2018-03-05 14:54:24 -0500

    Added WrongRole handling

--- utils/permissions.py
+++ utils/permissions.py
@@ -1,14 +1,23 @@
 from discord.ext import commands
 
-bot = None
 
-def is_owner_check(ctx):    
-    bot = ctx.bot
-    return str(ctx.message.author.id) in ctx.bot.config.get('OWNERS')
+class WrongRole(commands.CommandError):
+    """Thrown when user has wrong role for command."""
+    pass
+
+
+async def is_owner_check(ctx):
+    if str(ctx.author.id) in ctx.bot.config.get('OWNERS'):
+        return True
+    raise WrongRole(message="bot owner")
+
 
 def is_owner_or_gmod(ctx):
-    bot = ctx.bot
-    return (str(ctx.message.author.id) in ctx.bot.config.get('OWNERS')) or (str(ctx.message.author.id) in bot.config.get('GLOBAL_MODS'))
+    if (str(ctx.message.author.id) in ctx.bot.config.get('OWNERS')
+            or str(ctx.message.author.id) in ctx.bot.config.get('GLOBAL_MODS')):
+        return True
+    raise WrongRole(message="bot owner or global mod")
+
 
 def owner_id_check(bot, _id):
     return str(_id) in bot.config.get('OWNERS')
@@ -16,6 +25,7 @@ def owner_id_check(bot, _id):
 
 def owner():
     return commands.check(is_owner_check)
+
 
 def owner_or_gmod():
     return commands.check(is_owner_or_gmod)
