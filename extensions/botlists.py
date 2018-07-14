8ee1d5b3041eac10c7eb4967e4515431c65c4985
ry00001 <15631667+ry00001@users.noreply.github.com>
2018-07-14 14:42:36 +0100

    i dropped a /vote

--- extensions/botlists.py
+++ extensions/botlists.py
@@ -10,7 +10,7 @@ class BotLists:
     async def vote(self, ctx):
         await ctx.send(f'''If you like me and want to support my developer in aiming for certification, vote for me on DiscordBots.org!
 You may do so using this link:
-https://discordbots.org/bot/{ctx.me.id}
+https://discordbots.org/bot/{ctx.me.id}/vote
 Any vote helps!''')
 
 def setup(bot):
