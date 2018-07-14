0908b512042fe3ba34459ef61f9eea4d545c10bf
ry00001 <15631667+ry00001@users.noreply.github.com>
2018-07-14 14:40:57 +0100

    add botlist cog

---
+++ extensions/botlists.py
@@ -0,0 +1,17 @@
+import discord 
+from discord.ext import commands
+
+class BotLists:
+    # Commands for use with bot lists, eg DBots or DBL.
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def vote(self, ctx):
+        await ctx.send(f'''If you like me and want to support my developer in aiming for certification, vote for me on DiscordBots.org!
+You may do so using this link:
+https://discordbots.org/bot/{ctx.me.id}
+Any vote helps!''')
+
+def setup(bot):
+    bot.add_cog(BotLists(bot))
